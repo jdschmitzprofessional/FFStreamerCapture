108cf89e0a7d09ef08d7872047e9a4e6fd5cbdf6
jdschmitzprofessional <jdschmitzprofessional@gmail.com>
2020-07-24 23:15:12 -0700

    removed extra parameter

--- stream.py
+++ stream.py
@@ -7,7 +7,7 @@ import constants
 
 def stream(config):
     # instantiate the stream camera
-    stream = StreamCamera.StreamCamera(config=config, destination=constants.central_server)
+    stream = StreamCamera.StreamCamera(config=config)
     with open('/home/pi/log.txt', 'a+') as outfile:
         while True:
             # continuously records with a 2 second gap when interrupted.
